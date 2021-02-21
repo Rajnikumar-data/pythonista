247652bc04ed2e2019daf8777d9f2757eaf9c8f5
Rajnikumar-data <rajnikumarjobs@gmail.com>
2021-02-21 15:09:30 -0600

    thosrd-commit-levels of testing

--- Unit_testing.py
+++ Unit_testing.py
@@ -18,16 +18,19 @@ else:
     print('fail')
     
     
-### Easy Testing
-
+### Levels of Testing with given example
+# Function
 def upper_name(name):
     return name.upper()
 
-# In the above example data type(string) is required to perform the correct function. 
-# Therefore is string is the input then the test should behave correctly.
- # easy Test
-if upper_name("Rajni") == "RAJNI":
-    print('pass')
-else:
-    print('fail')
+# for easy testing: In the above example data type(string) is required to perform the correct function. 
+# Therefore if string is the input then the test should behave correctly otherwise crash.
+
+
  
+# For Medium testing: In the above example data type(string) is required to perform the correct function. 
+# if integer/boolean is the input then the test should still given the right output that is in this case it will crash.
+
+
+##Hard testing: any kind of input that may hack the function and effect the programme, user is guided immediately.
+
